8e37acd821883c45002cef6f49987f3e4e6400f4
Billones142 <stefanomerinoderui@gmail.com>
2023-10-20 22:37:15 -0300

    y sigo programando... todo culpa de juan este commit, de paso ya que estoy, feliz cum stiven

--- src/main/java/fulbo/ucp/SeleccionAFA.java
+++ src/main/java/fulbo/ucp/SeleccionAFA.java
@@ -1,5 +1,56 @@
 package fulbo.ucp;
 
+import java.util.ArrayList;
+
 public class SeleccionAFA {
-    
+    public SeleccionAFA(String pPresidente) {
+        super();
+        setPresidente(pPresidente);
+    }
+
+    private ArrayList<IntegranteSeleccion> seleccionado= new ArrayList<>();
+    private String presidente;
+
+    /********************Comienzo encapsulacion********************/
+    private void setPresidente(String presidente) {
+        this.presidente = presidente;
+    }
+
+    public String getPresidente() {
+        return presidente;
+    }
+
+    public void setSeleccionado(ArrayList<IntegranteSeleccion> seleccionado) {
+        this.seleccionado = seleccionado;
+    }
+
+    public ArrayList<IntegranteSeleccion> getSeleccionado() {
+        return seleccionado;
+    }
+    /**********************Fin encapsulacion**********************/
+
+    public void mostrarNomina() { //TODO
+        
+    }
+
+    public void agregarIntegrante(IntegranteSeleccion pIntegrante) { //TODO
+        getSeleccionado().add(pIntegrante);
+    }
+
+    public void quitarIntegrante(IntegranteSeleccion pIntegrante) { //TODO
+        for (int i = 0; i < getSeleccionado().size(); i++) {
+            if (getSeleccionado().get(i) == pIntegrante) {
+                getSeleccionado().remove(i);
+                break;
+            }
+        }
+    }
+
+    public void liquidarSueldos() { //TODO
+        
+    }
+
+    public void conocerRol(String pApellido) { //TODO
+        
+    }
 }
